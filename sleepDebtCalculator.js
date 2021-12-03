ec50beceeb817ddbc8a10ef63ad030d8d3291730
nanakwesimanuboafo <93545721+nanakwesimanuboafo@users.noreply.github.com>
2021-12-03 17:03:24 +0000

    Add files via upload

---
+++ sleepDebtCalculator.js
@@ -0,0 +1,64 @@
+const getSleepHours = day =>{
+    const getSleep = day.toLowerCase();
+    switch(getSleep){
+      case 'monday':
+        return 8;
+        break ;
+      case 'tuesday':
+        return 5;
+        break ;
+      case 'wednesday':
+        return 7;
+        break ;
+      case 'thursday':
+        return 9;
+        break ;
+      case 'friday':
+        return 5;
+        break ;
+      case 'saturday':
+        return 7;
+        break ;
+      case 'sunday':
+        return 8;
+        break ;
+      default: 
+      return 'Error!!'
+    }
+  };
+  
+  
+  const getActualSleepHours = () => 8 + 5 + 7 + 9 + 5 + 7 + 8;
+  
+  /*
+  const getActualSleepHours = () =>
+  getSleepHours('monday') + 
+  getSleepHours('tuesday') + 
+  getSleepHours('wednesday') + 
+  getSleepHours('thursday') + 
+  getSleepHours('friday') + 
+  getSleepHours('saturday') + 
+  getSleepHours('sunday');
+  */
+  
+  const getIdealSleepHours = idealHours => idealHours * 7;
+  
+  
+  const calculateSleepDebt = () => {
+    const actualSleepHours = getActualSleepHours();
+    const idealSleepHours = getIdealSleepHours(8);
+    if (actualSleepHours === idealSleepHours){
+      console.log('You got perfect amount of sleep');
+    } else if (actualSleepHours > idealSleepHours){
+      console.log('You got ' + (actualSleepHours - idealSleepHours) + ' more sleep');
+    } else if (actualSleepHours < idealSleepHours){
+      console.log('You need '  + (idealSleepHours-actualSleepHours) + ' more sleep');
+    }
+  };
+  
+  calculateSleepDebt();
+  
+  
+  
+  
+  
